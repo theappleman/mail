3365693811a86b531c6dab5dd20dd9849b471824
Daniel Cordero <maple.home@xxoo.ws>
2016-12-12 09:42:14 +0000

    acme: automate acmetool by using batch mode

--- lib/acme.pm
+++ lib/acme.pm
@@ -17,8 +17,40 @@ task "install", make {
 			ensure => "present";
 	}
 
+	file "/var/run/acme",
+		ensure => "directory",
+		owner => "root",
+		group => "root";
+	file "/var/run/acme/.well-known",
+		ensure => "directory",
+		owner => "root",
+		group => "root";
+	file "/var/run/acme/.well-known/acme-challenge",
+		ensure => "directory",
+		owner => "acme",
+		group => "acme";
+
+	file "/var/lib/acme",
+		ensure => "directory",
+		owner => "acme",
+		group => "acme";
+	file "/var/lib/acme/conf",
+		ensure => "directory",
+		owner => "acme",
+		group => "acme";
+
+	file "/var/lib/acme/conf/responses",
+		content => template('@response');
+
 	run "go get github.com/hlandau/acme/cmd/acmetool",
 		creates => "/home/acme/.local/go/bin/acmetool",
+		env => {
+			GOPATH => "/home/acme/.local/go",
+		},
+		user => "acme";
+
+	run "/home/acme/.local/go/bin/acmetool quickstart --batch",
+		creates => "/var/lib/acme/conf/target",
 		env => {
 			GOPATH => "/home/acme/.local/go",
 		},
@@ -39,4 +71,14 @@ User=acme
 Group=acme
 WorkingDirectory=/home/acme
 ExecStart=/home/acme/.local/go/bin/acmetool
+@end
+
+@response
+"acme-enter-email": "leca@xxoo.ws"
+"acme-agreement:https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf": true
+"acmetool-quickstart-choose-server": https://acme-v01.api.letsencrypt.org/directory
+"acmetool-quickstart-choose-method": webroot
+"acmetool-quickstart-webroot-path": "/var/run/acme/.well-known/acme-challenge"
+"acmetool-quickstart-complete": true
+"acmetool-quickstart-rsa-key-size": 4096
 @end
